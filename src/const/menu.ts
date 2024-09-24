7f2dd11a1e44da8ca43750d542d73734f679cf38
Wisnu Prayogo Kusumo <71598712+WisnuPrayogo096@users.noreply.github.com>
2024-09-24 15:24:24 +0700

    update

--- src/const/menu.ts
+++ src/const/menu.ts
@@ -1,36 +1,41 @@
-import {BetweenHorizonalStart, CircleGauge, LayoutDashboard, Users} from "lucide-react";
-import {Menu} from "@/types/menu-type";
+import {
+  BetweenHorizonalStart,
+  CircleGauge,
+  LayoutDashboard,
+  Users,
+} from "lucide-react";
+import { Menu } from "@/types/menu-type";
 
-const menus : Menu[] = [
-    {
-        icon: CircleGauge,
-        href: '/',
-        label: 'Dashboard',
-    },
-    {
-        icon: Users,
-        label: 'Antrean',
-        submenus: [
-            {label: "Ambil Antrean", href: '/components/test', active: true},
-            {label: "Loket Antrean", href: '/queue/locket', active: false},
-            {label: "Check-In Antrean", href: '#', active: false},
-            {label: "Daftar Antrean", href: '#', active: false},
-            {label: "Display Antrean", href: '#', active: false},
-        ]
-    },
-    {
-        icon: LayoutDashboard,
-        href: '/components',
-        label: 'Components',
-    },
-    {
-        icon: BetweenHorizonalStart,
-        label: 'Collapse Menu',
-        submenus: [
-            {label: "Submenu 1", href: '/components/test-page', active: false},
-            {label: "Submenu 2", href: '#', active: false},
-        ]
-    },
-]
+const menus: Menu[] = [
+  {
+    icon: CircleGauge,
+    href: "/",
+    label: "Dashboard",
+  },
+  {
+    icon: Users,
+    label: "Antrean",
+    submenus: [
+      { label: "Ambil Antrean", href: "/queue/polyclinic", active: true },
+      { label: "Loket Antrean", href: "/queue/counter", active: false },
+      { label: "Check-In Antrean", href: "#", active: false },
+      { label: "Daftar Antrean", href: "/queue/list", active: false },
+      { label: "Display Antrean", href: "#", active: false },
+    ],
+  },
+  {
+    icon: LayoutDashboard,
+    href: "/components",
+    label: "Components",
+  },
+  {
+    icon: BetweenHorizonalStart,
+    label: "Collapse Menu",
+    submenus: [
+      { label: "Submenu 1", href: "/components/test-page", active: false },
+      { label: "Submenu 2", href: "#", active: false },
+    ],
+  },
+];
 
-export {menus}
+export { menus };
